fb5a7f41d5f0eb0a64d9177b1a7f89df4c3530bf
163.beijing@gmail.com <163.beijing@gmail.com@ddf39ccc-7ea0-f36e-87c5-5259bbbb8561>
2011-09-16 06:36:06 +0000

    modify log

--- trunk/src/tcpcopy/address.c
+++ trunk/src/tcpcopy/address.c
@@ -14,7 +14,8 @@ void add_msg_connetion(uint16_t src_port,uint32_t dst_ip,uint16_t dst_port){
 
 int address_find_sock(uint16_t src_port){
 	if(addr[src_port].sock == 0){
-		logInfo(LOG_WARN,"it does not find address socket");
+		logInfo(LOG_WARN,"it does not find address socket:%u",
+				ntohs(src_port));
 		return -1;
 	}
 	return addr[src_port].sock;
@@ -22,7 +23,8 @@ int address_find_sock(uint16_t src_port){
 
 int address_copy_or_not(uint16_t src_port){
 	if(addr[src_port].sock == 0){
-		logInfo(LOG_INFO,"address socket is not valid");
+		logInfo(LOG_INFO,"address socket is not valid:%u",
+				ntohs(src_port));
 		return 0;
 	}
 	return 1;
@@ -30,7 +32,8 @@ int address_copy_or_not(uint16_t src_port){
 
 address *address_find_node(uint16_t src_port){
 	if(addr[src_port].sock == 0){
-		logInfo(LOG_INFO,"it does not find address socket pointer");
+		logInfo(LOG_INFO,"it does not find address socket pointer:%d",
+				ntohs(src_port));
 		return NULL;
 	}
 	return &addr[src_port];
