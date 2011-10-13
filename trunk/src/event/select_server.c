9a9d1f0328743902c8fbce0bf0d3269d7c25cf41
163.beijing@gmail.com <163.beijing@gmail.com@ddf39ccc-7ea0-f36e-87c5-5259bbbb8561>
2011-10-13 03:02:05 +0000

    fix address check  advised by redfox

--- trunk/src/event/select_server.c
+++ trunk/src/event/select_server.c
@@ -5,11 +5,12 @@
 #include <sys/select.h>
 
 #include "select_server.h"
+#include "../log/log.h"
 
 static int              	max_fd;
 static fd_set           	read_set;
 static select_server_func 	callback_func;
-static int              	valid_fds[1024];
+static int              	valid_fds[MAX_FD_NUM];
 static int              	fd_nums;
 
 
@@ -31,12 +32,26 @@ void select_sever_set_callback(select_server_func func){
  * =====================================================================================
  */
 void select_sever_add(int fd){
-	FD_SET(fd,&read_set);
-	if(fd > max_fd){
-		max_fd = fd;
+
+	if(fd > MAX_FD_VALUE)
+	{
+		logInfo(LOG_WARN,"fd:%d which more than MAX_FD_VALUE(default 1023)",fd);
+	}else
+	{
+		if(fd_nums >= MAX_FD_NUM)
+		{
+			logInfo(LOG_WARN,"too many fds");
+		}else
+		{
+			FD_SET(fd,&read_set);
+			if(fd > max_fd){
+				max_fd = fd;
+			}
+
+			valid_fds[fd_nums] = fd;
+			fd_nums++;
+		}
 	}
-	valid_fds[fd_nums] = fd;
-	fd_nums++;
 }
 
 
@@ -47,24 +62,27 @@ void select_sever_add(int fd){
  * =====================================================================================
  */
 void select_sever_del(int fd){
-	FD_CLR(fd,&read_set);
-	max_fd = 0;
-	int i = 0;
-	for(i=0;i<fd_nums;i++){
-		if(valid_fds[i] == fd){
-			int j=i;
-			while(j<fd_nums-1){
-				valid_fds[j] = valid_fds[j+1];
-				if(valid_fds[j] > max_fd){
-					max_fd = valid_fds[j];
+	if(fd <= MAX_FD_VALUE)
+	{
+		FD_CLR(fd,&read_set);
+		max_fd = 0;
+		int i = 0;
+		for(i=0;i<fd_nums;i++){
+			if(valid_fds[i] == fd){
+				int j=i;
+				while(j<fd_nums-1){
+					valid_fds[j] = valid_fds[j+1];
+					if(valid_fds[j] > max_fd){
+						max_fd = valid_fds[j];
+					}
+					j++;
 				}
-				j++;
+				fd_nums--;
+				break;
 			}
-			fd_nums--;
-			break;
-		}
-		if(valid_fds[i] > max_fd){
-			max_fd = valid_fds[i];
+			if(valid_fds[i] > max_fd){
+				max_fd = valid_fds[i];
+			}
 		}
 	}
 }
